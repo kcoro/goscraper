5e1b7cab048dde5703c68b8b2d1beef1252823c0
kcoro <6244044+kcoro@users.noreply.github.com>
2021-03-15 22:31:03 -0400

    fixed unicode explicit code in urls

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/kcoro/collyscraper
+module github.com/kcoro/jobscraper
 
 go 1.16
 
--- jobscraper.go
+++ jobscraper.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -44,6 +45,9 @@ func scrapeMonster(c *colly.Collector) {
 			Url:      url,
 		}
 
+		if location == "" {
+			location = reqData.Location
+		}
 		if title != "" && url != "" {
 			jobs = append(jobs, job)
 		}
@@ -56,7 +60,7 @@ func scrapeIndeed(c *colly.Collector) {
 		company := strings.TrimSpace(e.ChildText("span.company"))
 		location := strings.TrimSpace(e.ChildText("span.location"))
 		url := strings.TrimSpace(e.ChildAttr("a[href]", "href"))
-		url = "https://www.indeed.com/viewjob" + url
+		url = "https://indeed.com/pagead/clk?mo=r&" + url[17:]
 
 		job := Job{
 			Title:    title,
@@ -65,7 +69,10 @@ func scrapeIndeed(c *colly.Collector) {
 			Url:      url,
 		}
 
-		if title != "" && url != "" {
+		if location == "" {
+			location = reqData.Location
+		}
+		if title != "" || url != "" {
 			jobs = append(jobs, job)
 		}
 	})
@@ -78,18 +85,20 @@ func scrapeStack(c *colly.Collector) {
 		location := strings.TrimSpace(e.ChildText("span.fc-black-500"))
 		url := strings.TrimSpace(e.ChildAttr("a.s-link[href]", "href"))
 		url = "https://stackoverflow.com" + url
-
 		// Remove newline char and all chars after newline in company name
 		// Stack overflow returns extra chars.
-		companyBytes := []byte{}
-		for i := 0; i < len(company); i++ {
-			if byte(company[i]) != '\n' {
-				companyBytes = append(companyBytes, company[i])
-			} else {
-				break
-			}
-		}
-		companyFinal := string(companyBytes)
+		// companyBytes := []byte{}
+		// for i := 0; i < len(company); i++ {
+		// 	if byte(company[i]) != '\n' {
+		// 		companyBytes = append(companyBytes, company[i])
+		// 	} else {
+		// 		break
+		// 	}
+		// }
+		// companyFinal := string(companyBytes)
+
+		// Remove newline char and all chars after newline in company name
+		companyFinal := company[0:strings.Index(company, "\n")]
 
 		job := Job{
 			Title:    title,
@@ -98,6 +107,9 @@ func scrapeStack(c *colly.Collector) {
 			Url:      url,
 		}
 
+		if location == "" {
+			location = reqData.Location
+		}
 		if title != "" && url != "" {
 			jobs = append(jobs, job)
 		}
@@ -116,11 +128,22 @@ func errorHandler(c *colly.Collector) {
 	}
 }
 
-func HttpHandler(w http.ResponseWriter, r *http.Request) {
+// Example API request: ?title=software-developer&location=Miami-FL
+func Handler(w http.ResponseWriter, r *http.Request) {
 	// Get request query params from url
 	query := r.URL.Query()
 	reqData.Title = query.Get("title")
 	reqData.Location = query.Get("location")
+	// Indeed uses diff formatting, + between words not - like Monster and SO
+	indeedTitle := reqData.Title
+	indeedLocation := reqData.Location
+
+	// Remove space from request param and replace with %20 for querying job sites
+	strings.Replace(reqData.Title, " ", "-", -1)
+	strings.Replace(reqData.Location, " ", "-", -1)
+	// Remove - and replace with + for indeed search query
+	strings.Replace(indeedTitle, "-", "+", -1)
+	strings.Replace(indeedLocation, "-", "+", -1)
 
 	// Instantiate default collectors
 	cMonster := colly.NewCollector()
@@ -135,21 +158,29 @@ func HttpHandler(w http.ResponseWriter, r *http.Request) {
 	scrapeIndeed(cIndeed)
 	scrapeStack(cStack)
 
-	cMonster.Visit("https://www.monster.com/jobs/search/?q=" + reqData.Title + "&where=" + reqData.Location)
-	cIndeed.Visit("https://www.indeed.com/jobs?q=software+engineer&l=Raleigh,+NC&explvl=entry_level")
-	cStack.Visit("https://stackoverflow.com/jobs?q=Software+Engineer&l=North+Carolina%2C+USA&d=20&u=Miles")
-
-	// Print results for testing only
-	for i := 0; i < len(jobs); i++ {
-		printResults(jobs[i])
-	}
+	cMonster.Visit("https://www.monster.com/jobs/search/?q=" + reqData.Title + "&where=" + reqData.Location + "&intcid=skr_navigation_nhpso_searchMain")
+	cIndeed.Visit("https://www.indeed.com/jobs?q=" + indeedTitle + "&l=" + indeedLocation + "&explvl=entry_level")
+	cStack.Visit("https://stackoverflow.com/jobs?q=" + reqData.Title + "&l=" + reqData.Location + "USA&d=20&u=Miles")
 
 	// Encode map[string]Job to json
 	jobsJson, _ := json.Marshal(jobs)
-	fmt.Fprint(w, string(jobsJson)) // must explicityly convert json to string before sending
+	jobsJson = bytes.Replace(jobsJson, []byte("\\u0026"), []byte("&"), -1) // replace explicit unicode code with &
+	fmt.Fprint(w, string(jobsJson))                                        // must explicityly convert json to string before sending
+	jobs = make([]Job, 0, 200)                                             // Clear array of Jobs for next request
+
+	// jsonBytes := bytes.NewBuffer([]byte{})
+	// jsonEncoder := json.NewEncoder(jsonBytes)
+	// jsonEncoder.SetEscapeHTML(false)
+	// jsonEncoder.Encode(jobs)
+
+	// fmt.Fprint(w, jsonBytes.String()) // must explicitly convert bytes to string
+	// jobs = make([]Job, 0, 200)        // Clear array of Jobs for next request
+
+	// Close request
+	r.Body.Close()
 }
 
 func main() {
-	http.HandleFunc("/", HttpHandler)
+	http.HandleFunc("/", Handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
